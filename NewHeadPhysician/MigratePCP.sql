81210c0cd209518b055df73f749f7d36126c46af
Mintzmyer <samimintzmyer@gmail.com>
2017-11-10 14:31:24 -0800

    Empty file with comment

---
+++ NewHeadPhysician/MigratePCP.sql
@@ -0,0 +1,6 @@
+/* This query finds all patients whose PCP is a certain provider A and reassigns
+them to have a provider B as their PCP. It also sets an alert on their account
+ that although their PCP is provider B, they have not yet seen their new PCP*/
+
+
+
