f99f9b09da609d66cc9712543ca8d01a5badb6f2
Mintzmyer <samimintzmyer@gmail.com>
2017-12-14 09:20:35 -0800

    Updated preflight

--- InsuranceStatusAlert/PreflightOfferInsEnroll.sql
+++ InsuranceStatusAlert/PreflightOfferInsEnroll.sql
@@ -8,7 +8,7 @@
 */
 
 
--- patient_ table holds prim_insurance, sec_insurance columns
+-- person_payer table holds insurance
 -- person_ud table holds client defined options
 -- mstr_lists table holds list information: ud_demo1 is 'Insurance Enrollment Status'. For reference:
     -- Insured id = 6204564E...
@@ -16,10 +16,31 @@
     -- Unins NI id = E4740E1F...
     -- Unins Ref id = 484D32A8...
 
---Set Reference Variables
+        /***    SET REFERENCE VARIABLES     ***/
+
+DECLARE @AlertUninsuredSubj varchar(50)
+DECLARE @AlertUninsuredDesc varchar(50)
+
+DECLARE @AlertMedicaidSubj varchar(50)
+DECLARE @AlertMedicaidDesc varchar(50)
+
+DECLARE @AlertMedicareSubj varchar(50)
+DECLARE @AlertMedicareDesc varchar(50)
+
+SET @AlertUninsuredSubj = 'SHOP - Enroll in Insurance'
+SET @AlertUninsuredDesc = 'This patient does not have insurance.'
+
+SET @AlertMedicaidSubj = 'Medicaid - Insurance Alert'
+SET @AlertMedicaidDesc = 'This patient has Medicaid insurance.'
+
+SET @AlertMedicareSubj = 'Medicare - Insurance Alert'
+SET @AlertMedicareDesc = 'This patient has Medicare insurance.'
+
+
 DECLARE @Insured uniqueidentifier
 DECLARE @NotDoneYet uniqueidentifier
 DECLARE @Uninterested uniqueidentifier
+DECLARE @Referred uniqueidentifier
 
 SET @Insured = ( SELECT mstr_list_item_id
 	         FROM [NGProd].[dbo].mstr_lists
@@ -36,31 +57,68 @@ SET @Uninterested = ( SELECT mstr_list_item_id
 		 WHERE ( mstr_list_type = 'ud_demo1'
 			AND mstr_list_item_desc = 'Uninsured - Not Interested' ) )
 
+SET @Referred = ( SELECT mstr_list_item_id
+	         FROM [NGProd].[dbo].mstr_lists
+		 WHERE ( mstr_list_type = 'ud_demo1'
+			AND mstr_list_item_desc = 'Uninsured - Referred To SHOP' ) )
+
+
+-- Add Medicare/Medicaid payors to the list as needed:
+DECLARE @TrilliumMedicare     uniqueidentifier
+DECLARE @MedicareB            uniqueidentifier
+DECLARE @TrilliumMedicaid     uniqueidentifier
+DECLARE @DMAP                 uniqueidentifier
+
+DECLARE @Medicare TABLE (payor varchar(200))
+DECLARE @Medicaid TABLE (payor varchar(200))
+
+SET @TrilliumMedicare = ( SELECT payer_id
+	                  FROM [NGProd].[dbo].payer_mstr
+			  WHERE payer_name = 'Trillium Medicare' )
+
+SET @MedicareB = ( SELECT payer_id
+	                  FROM [NGProd].[dbo].payer_mstr
+			  WHERE payer_name = 'Medicare B' )
+
+SET @TrilliumMedicaid = ( SELECT payer_id
+	                  FROM [NGProd].[dbo].payer_mstr
+			  WHERE payer_name = 'Trillium Medicaid' )
+
+SET @DMAP = ( SELECT payer_id
+	                  FROM [NGProd].[dbo].payer_mstr
+			  WHERE payer_name = 'DMAP' )
+
+INSERT INTO @Medicare (payor) VALUES (@TrilliumMedicare), (@MedicareB)
+INSERT INTO @Medicaid (payor) VALUES (@TrilliumMedicaid), (@DMAP)
+
+
+        /****    STATUS UPDATES    ****/
+
 -- Set all insured patients to 'Insured' status
 UPDATE person_ud
 SET ud_demo1_id = @Insured
-FROM [NGProd].[dbo].person
-INNER JOIN person_ud
-ON person.person_id = person_ud.person_id
-INNER JOIN patient_
-on person.person_id = patient_.person_id
-WHERE ( patient_.prim_insurance is not NULL
-     OR patient_.sec_insurance is not NULL )
-     AND (person_ud.ud_demo1_id != @Insured 
-     OR person_ud.ud_demo1_id is NULL)
+FROM [NGProd].[dbo].person_ud
+INNER JOIN ( SELECT patient_.person_id FROM NGProd.dbo.patient_
+	     INTERSECT
+	     SELECT person_payer.person_id FROM NGProd.dbo.person_payer )
+AS insured ON person_ud.person_id = insured.person_id
+WHERE person_ud.ud_demo1_id != @Insured
 
--- Set all uninsured patients to 'Not Done Yet' status
+
+-- Set all uninsured patients to 'Not Done Yet' status 
+-- Only if they are Insured or NULL
+    -- Not if they are already Not Done Yet, Uninterested or already referred to shop
 UPDATE person_ud
 SET ud_demo1_id = @NotDoneYet
-FROM [NGProd].[dbo].person
-INNER JOIN person_ud
-ON person.person_id = person_ud.person_id
-INNER JOIN patient_
-on person.person_id = patient_.person_id
-WHERE ( patient_.prim_insurance is NULL
-     AND patient_.sec_insurance is NULL )
-     AND (person_ud.ud_demo1_id != @NotDoneYet 
-     OR person_ud.ud_demo1_id is NULL)
+FROM [NGProd].[dbo].person_ud
+INNER JOIN ( SELECT patient_.person_id FROM NGProd.dbo.patient_
+             EXCEPT
+             SELECT person_payer.person_id FROM NGProd.dbo.person_payer ) 
+AS uninsured ON person_ud.person_id = uninsured.person_id
+WHERE (    person_ud.ud_demo1_id != @NotDoneYet
+	OR person_ud.ud_demo1_id != @Uninterested
+	OR person_ud.ud_demo1_id != @Referred )
+
 
 --Insert Uninsured EPM Alert
 INSERT INTO [NGProd].[dbo].patient_alerts (
