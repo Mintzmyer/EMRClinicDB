d3949bc69cd6860c56ce0bad13ee3b16ea82ed4f
Mintzmyer <samimintzmyer@gmail.com>
2017-12-21 08:35:46 -0800

    Scaffolding files

---
+++ PatientTOC/VerifyPatientExists.sql
@@ -0,0 +1,7 @@
+/***
+* This script accepts a csv list of patient information, connects to our medical DB and checks for existence
+* 
+* If a patient is not found, this script will report it back.
+***/
+
+
