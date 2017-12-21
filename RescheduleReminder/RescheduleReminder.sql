3f4ac7b463d5d9256aa19281ea2696d87f5a7858
Mintzmyer <samimintzmyer@gmail.com>
2017-12-21 12:13:01 -0800

    Send MAs Task

--- RescheduleReminder/RescheduleReminder.sql
+++ RescheduleReminder/RescheduleReminder.sql
@@ -1,7 +1,7 @@
 /***
 * This script queries the database for cancelled/missed appointments that need to be rescheduled
 *
-* 
+* It then creates a task and sends it to the medical assistants, so they assess medical urgency and coordinate with scheduling staff
 ***/
 
 
