f0f61aa71461316b92172b56d7fc2ea5762f240b
Mintzmyer <samimintzmyer@gmail.com>
2017-12-27 09:41:40 -0800

    Found EPM tasks, now for EHR tasks

--- RescheduleReminder/RescheduleReminder.sql
+++ RescheduleReminder/RescheduleReminder.sql
@@ -4,5 +4,9 @@
 * It then creates a task and sends it to the medical assistants, so they assess medical urgency and coordinate with scheduling staff
 ***/
 
+-- Appointments are in table: dbo.appointments
+-- EPM Tasks are in table: dbo.task_mstr
+-- EHR Tasks are in table: dbo.
+
 
 
